29bf513d2c297a5956374d260c3bc1c40ef569ef
Muflih majid <bsi80113@bsi.co.id>
2024-02-12 13:40:58 +0700

    first commit

---
+++ src/main/java/com/CleanArc/ProjectNasa/ProjectNasaApplication.java
@@ -0,0 +1,13 @@
+package com.CleanArc.ProjectNasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CleanArc/ProjectNasa/ProjectNasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CleanArc.ProjectNasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
